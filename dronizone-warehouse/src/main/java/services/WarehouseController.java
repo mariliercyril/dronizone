a4ba54c6f9d57f733af06db32523e508933972f9
Romain garnier <romain.garnier@etu.unice.fr>
2019-09-30 18:14:56 +0200

    Mise en place de Spring boot web

--- dronizone-warehouse/src/main/java/services/WarehouseController.java
+++ dronizone-warehouse/src/main/java/services/WarehouseController.java
@@ -3,12 +3,15 @@ package services;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/warehouse")
 public class WarehouseController {
 
     @GetMapping("/connected")
