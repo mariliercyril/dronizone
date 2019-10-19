c8203c2a2331f646328872be254cff590df6612d
ValentinAh-Kane <valentin.ah-kane@etu.univ-cotedazur.fr>
2019-10-19 12:57:41 +0200

    #22 #2 Methods for WarehouseController that will be used by the OrderService

--- dronizone-warehouse/src/main/java/com/scp/dronizone/warehouse/service/WarehouseController.java
+++ dronizone-warehouse/src/main/java/com/scp/dronizone/warehouse/service/WarehouseController.java
@@ -4,6 +4,8 @@ import com.scp.dronizone.common.entity.Item;
 import com.scp.dronizone.common.entity.Order;
 import com.scp.dronizone.common.entity.OrderManager;
 import com.scp.dronizone.common.entity.Warehouse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/warehouse")
 public class WarehouseController {
+    private static final Logger LOG = LoggerFactory.getLogger(WarehouseController.class);
 
     @GetMapping("/connected")
     public String connected() {
@@ -22,9 +25,22 @@ public class WarehouseController {
         return OrderManager.getUnpackedOrders();
     }
 
+    @GetMapping("/addItem")
+    public void addItem(@RequestParam(value = "itemId", required = true) String itemId) {
+        LOG.warn("Adding item with id : " + itemId);
+        Warehouse.addItem(new Item(itemId));
+        LOG.warn("Item list now : " + Warehouse.getItems().size());
+    }
+
+
     @RequestMapping("/pack")
     public void packOrder(@RequestParam(value = "id", required = true) String idOrder){
         OrderManager.setOrderPacked(idOrder);
+    }
+
+    @RequestMapping("/items")
+    public List<Item> getAvailableItems(){
+        return Warehouse.getItems();
     }
 
 }
