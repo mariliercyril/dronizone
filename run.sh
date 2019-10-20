e107d90fbc8079e3e154cd6b02caf135e2f2f79b
Romain GARNIER <romain.garnier@etu.unice.fr>
2019-10-20 19:37:48 +0200

    #24 et update de run.sh

--- run.sh
+++ run.sh
@@ -4,20 +4,33 @@
 
 #cd dronizone-order
 #./mvnw exec:java
+
+#RUN SERVICES
+docker run -d -p 9001:9001 -t scp1920/dronizone:order
+docker run -d -p 9002:9002 -t scp1920/dronizone:warehouse
 docker run -d -p 9003:9003 -t scp1920/dronizone:notification
+docker run -d -p 9004:9004 -t scp1920/dronizone:fleet
+
 sleep 10;
 cd dronizone-order
 mvn test
-
+cd ..
 
 #cd ../dronizone-warehouse
 #./mvnw exec:java
+sleep 10;
+cd dronizone-warehouse
+mvn test
+cd ..
 
 #cd ..
-docker run -d -p 9003:9003 -t scp1920/dronizone:notification
 sleep 10;
 cd dronizone-notification
 gnome-terminal -- ./notification_service_tests_launcher.sh
+cd ..
 
 #cd dronizone-fleet
 #./mvnw exec:java
+sleep 10;
+cd dronizone-notification
+mvn test
